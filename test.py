c784f690827c393c86e28bd53af014caac298c97
matthieu <matthieu.caron@laposte.net>
2016-02-19 11:33:04 +0100

    fini

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import Graph as g 
 import matplotlib.pyplot as plt
 import time
+import numpy as np
 
 graph = g.graphAlea(5, 5, 0.5)
 
@@ -46,9 +47,38 @@ def moyenne50(n,p,algo) :
     sommeC = sum(couleurs)
     return (float(sommeT)/50.,float(sommeC)/50)
 
-print(moyenne50(50,0.5,0))
-print(moyenne50(50,0.5,1))
-print(moyenne50(50,0.5,2))
+def plotColor() :
+    plt.xlabel('probabilité')
+    x = np.linspace(0.1,0.9,num=9)
+    plt.ylabel('colorations')
+    y = np.zeros((3,9))
+    for i in range(3) :
+        for j in range(9) :
+            y[i,j] = moyenne50(100,x[j],i)[1]
+    labels=["algoNaif","algoDSATUR","algoWelshPowel"]
+    for y_arr, label in zip(y,labels) :
+        plt.plot(x,y_arr,label=label)
+    plt.legend()
+    plt.show()
+
+def plotTemps() :
+    plt.xlabel('probabilité')
+    x = np.linspace(0.1,0.9,num=9)
+    plt.ylabel('temps')
+    y = np.zeros((3,9))
+    for i in range(3) :
+        for j in range(9) :
+            y[i,j] = moyenne50(100,x[j],i)[0]
+    labels=["algoNaif","algoDSATUR","algoWelshPowel"]
+    for y_arr, label in zip(y,labels) :
+        plt.plot(x,y_arr,label=label)
+    plt.legend()
+    plt.show()
+
+plotTemps()
+# print(moyenne50(50,0.5,0))
+# print(moyenne50(50,0.5,1))
+# print(moyenne50(50,0.5,2))
 
 
 
